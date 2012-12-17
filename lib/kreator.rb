e9eb23e8ace703d3bd1d56d5e6335676ee6a9e36
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2012-12-17 23:08:41 +0100

    ERB template

--- lib/kreator.rb
+++ lib/kreator.rb
@@ -1,5 +1,10 @@
 class Kreator
   def call(env)
-    Rack::Response.new("Hello!")
+    Rack::Response.new(render('index.html.erb'))
+  end
+
+  def render(template)
+    path = File.expand_path("../views/#{template}", __FILE__)
+    ERB.new(File.read(path)).result(binding)
   end
 end
